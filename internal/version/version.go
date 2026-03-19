197c196b230fee068e090aebd394b265598aeff5
Matt Cockayne <matthew.cockayne@deline8.ai>
2026-03-19 11:34:23 +0000

    fix(version): rename vars to match goreleaser ldflags and enrich dev version from vcs info

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,40 +3,46 @@ package version
 import (
 	"runtime/debug"
 
-	"github.com/phpboyscout/gtb/pkg/version"
+	pkgversion "github.com/phpboyscout/gtb/pkg/version"
 )
 
 var (
-	v = "dev"
-	c = "none"
-	d = "unknown"
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
 )
 
 func init() {
-	if v != "dev" {
-		return
-	}
-
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
 		return
 	}
 
-	if info.Main.Version != "(devel)" {
-		v = info.Main.Version
-	}
-
 	for _, setting := range info.Settings {
 		switch setting.Key {
 		case "vcs.revision":
-			c = setting.Value
+			commit = setting.Value
 		case "vcs.time":
-			d = setting.Value
+			date = setting.Value
+		case "vcs.modified":
+			if setting.Value == "true" {
+				commit += "-dirty"
+			}
+		}
+	}
+
+	// Only override version if it's still the default "dev"
+	if version == "dev" {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			version = info.Main.Version
+		} else {
+			// Fallback to the short commit hash if no tag exists
+			version = commit
 		}
 	}
 }
 
-// Get returns the current version information as a pkg/version.Info.
-func Get() version.Info {
-	return version.NewInfo(v, c, d)
+// Get returns the current version information as a pkgversion.Info.
+func Get() pkgversion.Info {
+	return pkgversion.NewInfo(version, commit, date)
 }
